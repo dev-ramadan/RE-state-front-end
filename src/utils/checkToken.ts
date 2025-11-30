fc60a4a8856072b11c85c5017090bb03d4013bb3
dev-ramadan <ganna123adam@gmail.com>
2025-11-30 22:27:11 +0200

    TEST

--- src/utils/checkToken.ts
+++ src/utils/checkToken.ts
@@ -1,26 +1,12 @@
 import { jwtDecode } from "jwt-decode";
 
-export function isTokenExpired(token: string) {
+export function isTokenExpired(token: string | null): boolean {
+  if (!token) return true;
   try {
     const decoded: any = jwtDecode(token);
-    const now = Date.now() / 1000;
+    const now = Math.floor(Date.now() / 1000);
     return decoded.exp < now;
   } catch (err) {
     return true;
   }
 }
-
-export const getRole = async (id: string) => {
-  const token = localStorage.getItem("token");
-  const res = await fetch(`https://re-estate.runasp.net/api/UserRole/user-roles/${id}`, {
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
-
-    }
-  })
-  const role = await res.json();
-
-  return role.data
-}
-
