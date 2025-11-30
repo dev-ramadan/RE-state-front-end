6f2b5b0e6c1f84dcbb53d229dc5b0da17e458d84
dev-ramadan <ganna123adam@gmail.com>
2025-11-30 23:16:55 +0200

    TEST

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,19 +4,30 @@ import Input from '../../ui/Input';
 import InputField from '../../ui/InputField';
 import Button from '../../ui/Button';
 import CardDetils from '../../ui/CardDetils';
-import { useAuth } from '../../context/AuthContext';
+import { useEffect, useState } from 'react';
 import { useUserProfile } from '../../hooks/useProfile';
+import { useAuth } from '../../context/AuthContext';
 import { destructUserProfile } from '../../utils/helper';
-import { Link } from 'react-router';
 
 const Main = () => {
-  const { user,search,setSearch } = useAuth()
-  const { data } = useUserProfile();
-  const { listings } = destructUserProfile(data);
-  
-  const foreSale = listings.filter(item => item.propertyPurpose === "Sale");
-  const foreRent = listings.filter(item => item.propertyPurpose === "Rent");
-  if(!user) return("user not found");
+  const { user, search, setSearch } = useAuth();
+  const { data, isLoading } = useUserProfile();
+
+  const [sale, setSale] = useState<any[]>([]);
+  const [rent, setRent] = useState<any[]>([]);
+  const [list, setList] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const { listings } = destructUserProfile(data) || [];
+
+    setList(listings);
+    setSale(listings.filter((item:any) => item.propertyPurpose === "Sale"));
+    setRent(listings.filter((item:any) => item.propertyPurpose === "Rent"));
+  }, [data]);
+
+  if (isLoading) return <p className="text-center py-20">Loading...</p>;
   return (
     <main className="">
       {/* top */}
@@ -35,55 +46,49 @@ const Main = () => {
           {/* Search */}
           <div className="w-[80%] mx-auto">
             <InputField id="search">
-            <Link to={'/search'}>
               <Button
                 children={'Search'}
                 icon={<Search />}
                 className="flex absolute w-fit right-3 p-2 top-4"
-                />
-                </Link>
+              />
               <Input
                 type="text"
                 id="search"
                 name="search"
                 placeholder="Search"
-                value={search}
-                onChange={(e)=>setSearch(e.target.value)}
                 className="mb-16 p-6 bg-white placeholder:text-gray-500  placeholder:md:text-lg text-black border-none outline-none"
               />
             </InputField>
           </div>
 
           {/* Card */}
-          {
-            user?.role == "Agent" &&
+          {user?.role == "Agent" &&
             <div className="relative">
               <div className="flex justify-around items-center absolute w-full -top-10 flex-wrap gap-2.5 mb-4 md:mb-0">
                 <CardDetils
                   Hfont="bold"
                   Pfont="semibold"
-                  count={listings.length}
+                  count={list.length}
                   title="Properties Listed"
                   Hcolor="blue-700"
                 />
                 <CardDetils
                   Hfont="bold"
                   Pfont="semibold"
-                  count={foreSale.length}
+                  count={sale.length}
                   title="For Sale"
                   Hcolor="blue-700"
                 />
                 <CardDetils
                   Hfont="bold"
                   Pfont="semibold"
-                  count={foreRent.length}
+                  count={rent.length}
                   title="For Rent"
                   Hcolor="blue-700"
                 />
               </div>
             </div>
           }
-
         </div>
       </div>
     </main>
